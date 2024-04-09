ae3c94f0088a83b862b30058fe6f51e0051a5506
1561193 <164223652+1561193@users.noreply.github.com>
2024-04-09 19:46:22 +0300

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,6 +1,6 @@
-num_1 = 5
-num_2 = 3
-sum_1 = (num_1 + num_2)
-print(sum_1)
-result = (sum_1 + num_1 * num_2)
+num1 = 5
+num2 = 3
+sum = (num1 + num2)
+print(sum)
+result = (sum + num1 * num2)
 print(result)
